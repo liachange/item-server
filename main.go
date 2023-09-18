b55575bc7b855b542e09e4722a3b295511002268
liachange <caojian816@163.com>
2023-09-18 17:35:52 +0800

    集成 gin

--- main.go
+++ main.go
@@ -1,7 +1,23 @@
 package main
 
-import "fmt"
+import (
+	"github.com/gin-gonic/gin"
+	"net/http"
+)
 
 func main() {
-	fmt.Println("Hello word!")
+	// 初始化 Gin 实例
+	r := gin.Default()
+
+	// 注册一个路由
+	r.GET("/", func(c *gin.Context) {
+
+		// 以 JSON 格式响应
+		c.JSON(http.StatusOK, gin.H{
+			"Hello": "World!",
+		})
+	})
+
+	// 运行服务
+	r.Run()
 }
