971c68f2956245d8091d3925b9de9556b1420bfa
liachange <caojian816@163.com>
2023-09-18 22:04:04 +0800

    配置方案的实现

--- main.go
+++ main.go
@@ -1,40 +1,36 @@
 package main
 
 import (
+	"flag"
+	"fmt"
 	"github.com/gin-gonic/gin"
-	"net/http"
-	"strings"
+	"item-server/bootstrap"
+	btsConfig "item-server/config"
+	"item-server/pkg/config"
 )
 
+func init() {
+	// 加载 config 目录下的配置信息
+	btsConfig.Initialize()
+}
+
 func main() {
-	// 初始化 Gin 实例
-	r := gin.New()
-	//注册中间件
-	r.Use(gin.Logger(), gin.Recovery())
-	// 注册一个路由
-	r.GET("/", func(c *gin.Context) {
+	// 配置初始化，依赖命令行 --env 参数
+	var env string
+	flag.StringVar(&env, "env", "", "加载 .env 文件，如 --env=testing 加载的是 .env.testing 文件")
+	flag.Parse()
+	config.InitConfig(env)
 
-		// 以 JSON 格式响应
-		c.JSON(http.StatusOK, gin.H{
-			"Hello": "World!",
-		})
-		// 处理 404 请求
-		r.NoRoute(func(c *gin.Context) {
-			// 获取标头信息的 Accept 信息
-			acceptString := c.Request.Header.Get("Accept")
-			if strings.Contains(acceptString, "text/html") {
-				// 如果是 HTML 的话
-				c.String(http.StatusNotFound, "页面返回 404")
-			} else {
-				// 默认返回 JSON
-				c.JSON(http.StatusNotFound, gin.H{
-					"error_code":    404,
-					"error_message": "路由未定义，请确认 url 和请求方法是否正确。",
-				})
-			}
-		})
-	})
+	// new 一个 Gin Engine 实例
+	router := gin.New()
 
-	// 运行服务，默认为 8080，我们指定端口为 8000
-	r.Run(":8000")
+	// 初始化路由绑定
+	bootstrap.SetupRoute(router)
+
+	// 运行服务
+	err := router.Run(":" + config.Get("app.port"))
+	if err != nil {
+		// 错误处理，端口被占用了或者其他错误
+		fmt.Println(err.Error())
+	}
 }
