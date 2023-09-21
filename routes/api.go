28e7b3fcc9013c7cc47dfa678973222b7ff3807c
liachange <caojian816@163.com>
2023-09-21 17:01:26 +0800

    API 域名和前缀

--- routes/api.go
+++ routes/api.go
@@ -4,13 +4,19 @@ import (
 	"github.com/gin-gonic/gin"
 	"item-server/app/http/controllers/api/v1/auth"
 	"item-server/app/http/middlewares"
+	"item-server/pkg/config"
 )
 
 // RegisterAPIRoutes 注册网页相关路由
 func RegisterAPIRoutes(r *gin.Engine) {
 
 	// 测试一个 v1 的路由组，我们所有的 v1 版本的路由都将存放到这里
-	v1 := r.Group("/v1")
+	var v1 *gin.RouterGroup
+	if len(config.Get("app.api_domain")) == 0 {
+		v1 = r.Group("/api/v1")
+	} else {
+		v1 = r.Group("/v1")
+	}
 	{
 		authGroup := v1.Group("/auth")
 		// 限流中间件：每小时限流，作为参考 Github API 每小时最多 60 个请求（根据 IP）
